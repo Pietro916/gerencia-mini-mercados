ac2e761299db3c0ac64e6e9b676f2d619c4abf40
rm87874 <rm87874@BF-P3L4-M15.fieb.local>
2023-02-16 21:45:46 -0300

    commit

---
+++ gerencia-mini-mercados/src/test/java/com/fieb/gerenciaminimercados/GerenciaMiniMercadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fieb.gerenciaminimercados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaMiniMercadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
